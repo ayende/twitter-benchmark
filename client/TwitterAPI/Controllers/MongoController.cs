2fe04278c15021609eab7a7cf42a1bb86d6cd803
Ayende Rahien <ayende@ayende.com>
2023-06-14 12:49:16 +0300

    Avoid serialization in Mongo

--- client/TwitterAPI/Controllers/MongoController.cs
+++ client/TwitterAPI/Controllers/MongoController.cs
@@ -3,6 +3,8 @@ using MongoDB.Driver;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TwitterAPI;
+using System;
+using MongoDB.Bson.IO;
 
 namespace TwitterAPI.Controllers
 {
@@ -10,66 +12,77 @@ namespace TwitterAPI.Controllers
     [Route("[controller]/[action]")]
     public class MongoController : Controller
     {
-        private readonly IMongoCollection<TweetModel> _tweets;
-        private readonly IMongoCollection<UserModel> _users;
+        private readonly IMongoCollection<BsonDocument> _tweets;
+        private readonly IMongoCollection<BsonDocument> _users;
 
-        public MongoController(IMongoCollection<TweetModel> tweets, IMongoCollection<UserModel> users)
+        public MongoController(Tuple<IMongoCollection<BsonDocument>, IMongoCollection<BsonDocument>> i)
         {
-            _tweets = tweets;
-            _users = users;
+            _tweets = i.Item1;
+            _users = i.Item2;
         }
 
         [HttpGet]
         public async Task<ActionResult> Users(long id)
         {
-            var filter = Builders<UserModel>.Filter.Eq("_id", "users/" + id);
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", "users/" + id);
             var document = await _users.Find(filter).FirstOrDefaultAsync();
             if (document == null)
             {
                 return NotFound();
             }
-            return Json(document);
+            return new ContentResult
+            {
+                Content = document.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.RelaxedExtendedJson }),
+                ContentType = "application/json"
+            };
         }
 
         [HttpGet]
         public async Task<ActionResult> Tweet(long id)
         {
-            var filter = Builders<TweetModel>.Filter.Eq("_id", "tweets/" + id);
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", "tweets/" + id);
             var document = await _tweets.Find(filter).FirstOrDefaultAsync();
             if (document == null)
             {
                 return NotFound();
             }
-            return Json(document);
+            return new ContentResult
+            {
+                Content = document.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.RelaxedExtendedJson }),
+                ContentType = "application/json"
+            };
         }
 
         [HttpGet]
         public async Task<ActionResult> TweetsFor(long id, int page = 0)
         {
-            var filter = Builders<TweetModel>.Filter.Eq("user", "users/" + id);
+            var filter = Builders<BsonDocument>.Filter.Eq("user", "users/" + id);
             var results = await _tweets.Find(filter)
-                .SortByDescending(x => x.CreatedAt)
+                .Sort(Builders<BsonDocument>.Sort.Descending("created_at"))
                 .Limit(15)
                 .Skip(page * 15)
-                .Project(x => new
-                {
-                    x.User,
-                    x.CreatedAt,
-                    x.FavoriteCount,
-                    x.Geo,
-                    x.InReplyToScreenName,
-                    x.Place,
-                    x.FullText,
-                    x.ReplyCount,
-                    x.RetrievedUtc,
-                    x.Source,
-                    x.Language,
-                    x.SupplementalLanguage,
-                    x.PossiblySensitive,
-                    x.ConversationId,
-                })
+                .Project(Builders<BsonDocument>.Projection
+                    .Include("user")
+                    .Include("created_at")
+                    .Include("favorite_count")
+                    .Include("geo")
+                    .Include("in_reply_to_screen_name")
+                    .Include("place")
+                    .Include("full_text")
+                    .Include("reply_count")
+                    .Include("retrieved_utc")
+                    .Include("source")
+                    .Include("lang")
+                    .Include("supplemental_language")
+                    .Include("possibly_sensitive")
+                    .Include("conversation_id")
+                 )
                 .ToListAsync();
-            return Json(results);
+            return new ContentResult
+            {
+                Content = results.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.RelaxedExtendedJson }),
+                ContentType = "application/json"
+            };
         }
     }
 }
